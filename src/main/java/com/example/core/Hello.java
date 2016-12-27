a331a4fbbc9230dea136458f16d6aa2e8cf7d337
kpatel53 <patelkrupag@gmail.com>
2016-12-26 22:28:50 -0800

    changes

--- src/main/java/com/example/core/Hello.java
+++ src/main/java/com/example/core/Hello.java
@@ -1,5 +1,11 @@
 package com.example.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
